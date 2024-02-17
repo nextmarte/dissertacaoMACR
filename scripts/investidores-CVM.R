9c7873950223577ee0c633a6b8c4ad9f449232a5
Marcus_Ramalho <ramalhomarcusantonio@id.uff.br>
2024-02-17 01:52:29 -0300

    Refactor download_and_read function to improve readability and error handling

--- scripts/investidores-CVM.R
+++ scripts/investidores-CVM.R
@@ -4,30 +4,55 @@ library(tidyverse)
 # Function to download and read data
 download_and_read <- function(year) {
     # Build the URL
-    url <- paste0("https://dados.cvm.gov.br/dados/FII/DOC/INF_MENSAL/DADOS/inf_mensal_fii_", year, ".zip")
+    url <- paste0(
+        "https://dados.cvm.gov.br/dados/FII/DOC/INF_MENSAL/DADOS/inf_mensal_fii_",
+        year, ".zip"
+    )
 
     # Download the zip file
     zip_file <- paste0("inf_mensal_fii_", year, ".zip")
     download.file(url, destfile = zip_file)
 
     # Read data from zip files with error logging
-    data_ativo_passivo <- tryCatch(read_delim(unz(zip_file,
-     paste0("inf_mensal_fii_ativo_passivo_", year, ".csv")),
-      delim = ";", locale = locale(encoding = "ISO-8859-1"),
-       show_col_types = FALSE), error = function(e) {
+    data_ativo_passivo <- tryCatch(read_delim(
+        unz(
+            zip_file,
+            paste0("inf_mensal_fii_ativo_passivo_", year, ".csv")
+        ),
+        delim = ";", locale = locale(encoding = "ISO-8859-1"),
+        show_col_types = FALSE
+    ), error = function(e) {
         message(
             "Error reading ativo_passivo data for year ",
             year, ": ", e
         )
-        return(NULL)
+        return()
     })
-    data_complemento <- tryCatch(read_delim(unz(zip_file, paste0("inf_mensal_fii_complemento_", year, ".csv")), delim = ";", locale = locale(encoding = "ISO-8859-1"), show_col_types = FALSE), error = function(e) {
-        message("Error reading complemento data for year ", year, ": ", e)
-        return(NULL)
+    data_complemento <- tryCatch(read_delim(
+        unz(
+            zip_file,
+            paste0("inf_mensal_fii_complemento_", year, ".csv")
+        ),
+        delim = ";", locale = locale(encoding = "ISO-8859-1"),
+        show_col_types = FALSE
+    ), error = function(e) {
+        message(
+            "Error reading complemento data for year ", year, ": ", e
+        )
+        return()
     })
-    data_geral <- tryCatch(read_delim(unz(zip_file, paste0("inf_mensal_fii_geral_", year, ".csv")), delim = ";", locale = locale(encoding = "ISO-8859-1"), show_col_types = FALSE), error = function(e) {
-        message("Error reading geral data for year ", year, ": ", e)
-        return(NULL)
+    data_geral <- tryCatch(read_delim(
+        unz(
+            zip_file,
+            paste0("inf_mensal_fii_geral_", year, ".csv")
+        ),
+        delim = ";", locale = locale(encoding = "ISO-8859-1"),
+        show_col_types = FALSE
+    ), error = function(e) {
+        message(
+            "Error reading geral data for year ", year, ": ", e
+        )
+        return()
     })
 
     # Remove the zip file
@@ -37,17 +62,17 @@ download_and_read <- function(year) {
 }
 
 # Define the range of years
-years <- 2016:2024
+years <- seq(2016, 2024)
 
-# Use purrr::map instead of a for loop
+# Use purrr::map for loop over the years
 data_list <- map(years, download_and_read)
 
 # Add data to the list
 data_list <- map(data_list, function(data) {
     list(
-        ativo_passivo = if (!is.null(data$data_ativo_passivo)) data$data_ativo_passivo else NULL,
-        geral = if (!is.null(data$data_geral)) data$data_geral else NULL,
-        complemento = if (!is.null(data$data_complemento)) data$data_complemento else NULL
+        ativo_passivo = data$data_ativo_passivo,
+        geral = data$data_geral,
+        complemento = data$data_complemento
     )
 })
 
@@ -56,4 +81,3 @@ complemento_data <- map(data_list, "complemento") %>% bind_rows()
 
 # Return the data frames instead of assigning them to the global environment
 list(data_list = data_list, complemento_data = complemento_data)
-
